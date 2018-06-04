2d055bcd80c5e8a2ff0dcd78d8e3893014e0094c
Tommy Svensson <tommykajsvensson@gmail.com>
2018-06-04 22:35:41 +0200

    connect express server with next.js

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Index = (props) => (
+  <h1>Next Social Network</h1>
+);
+
+export default Index;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const session = require('express-session');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const server = express();
+
+app.prepare().then(() => {
+  server.use(session({
+    secret: 'secret',
+    saveUninitialized: false,
+    resave: true
+  }));
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  /* eslint-disable no-console */
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('Server listening at http://localhost:3000');
+  });
+});
